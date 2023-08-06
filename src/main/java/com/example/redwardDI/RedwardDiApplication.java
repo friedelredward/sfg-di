83de4368a4bda25238ebc2a4e15e19cf77f38f90
R Edward Friedel <redwardweb@gmail.com>
2023-08-06 18:05:03 +0200

    added initial project structure from initializr

---
+++ src/main/java/com/example/redwardDI/RedwardDiApplication.java
@@ -0,0 +1,13 @@
+package com.example.redwardDI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedwardDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedwardDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/redwardDI/RedwardDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redwardDI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedwardDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
